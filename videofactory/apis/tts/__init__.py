f6a018700c4b862a27a3def066004ff9dfd6b973
HasegawaSkip <hasegawaskip@gmail.com>
2023-07-19 08:58:28 +0700

    added text: str type hint

--- videofactory/apis/tts/__init__.py
+++ videofactory/apis/tts/__init__.py
@@ -2,5 +2,5 @@ class TextToSpeech:
     def __init__(self, provider):
         self.provider = provider
 
-    def generate_audio(self, text, **kwargs):
+    def generate_audio(self, text: str, **kwargs):
         raise NotImplementedError
