566ea6fa15d45907e2374c62905c98e9c36b292e
HasegawaSkip <hasegawaskip@gmail.com>
2023-07-25 14:09:37 +0700

    added run.bat [Initial commit] - with initialization message and delay

---
+++ run.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Initializing VideoFactory...
+
+rem Delay for 2 seconds
+timeout /t 2 > nul
+
+echo ----------------------------
+echo.
+echo VideoFactory initialized successfully.
+echo.
+echo --------------------------------------
+echo.
+
+rem Run the main.py script
+python main.py
